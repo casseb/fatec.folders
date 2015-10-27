7e999778ee68ff9cbf84f9fa146c16c90f81da83
Casseb <felipe.casseb@gmail.com>
2015-10-27 21:27:51 -0200

    Fim da aulda de La do dia 27/10

--- Semestre1/alunos_dicionario.py
+++ Semestre1/alunos_dicionario.py
@@ -1,7 +1,28 @@
+#Função que pega String e converte em um dicionario
+def converteStringDicionario(string,separadorLinha,separadorIndice,separadorConteudo):
+    resultado = {}
+    linha = string.rstrip().split(separadorLinha)
+    for conteudo in linha:
+        conteudoVariavel = conteudo[conteudo.index(separadorIndice)+1:].split(separadorConteudo)
+        if(len(conteudoVariavel)==1):
+            resultado[conteudo[:conteudo.index(separadorIndice)]] = conteudoVariavel[0]
+        else:
+            conteudoTotal = []
+            for conteudoTrabalhar in conteudoVariavel:
+                conteudoTotal.append(conteudoTrabalhar)
+            resultado[conteudo[:conteudo.index(separadorIndice)]] = conteudoTotal
+    return resultado
+
+
 #Adquirindo dados do arquivo
 arquivo = open('dados.txt')
 linhas = arquivo.read()
 arquivo.close()
 
-print(linhas)
+#Criando dicionário de dados
+alunoString = linhas.rstrip().split("#")
+print(converteStringDicionario(alunoString[0],"\n",":",","))
+
+
+
 
